3b86c540c5abc30ac395a48474a74179e0d02fd1
Kacper Dąbrowski <traviankacper@wp.pl>
2019-11-06 14:26:04 +0100

    1_5UnitTests

---
+++ CsharpStrukturyGeneryczne/_1TypyGeneryczneTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("_1TypyGeneryczneTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_1TypyGeneryczneTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("441b4143-8c2a-470f-9133-499399cc9fe1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
